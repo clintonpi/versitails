761d24e77a41d7ad800616195ee06ae25b6a058d
clintonpi <nkwochaclinton@gmail.com>
2019-04-24 08:00:17 +0100

    FIX(CLIENT): ENSURE THAT THE HOME COMPONENT IS NOT ALREADY SCROLLED WHEN RENDERED
    This happens (on smaller screen sizes) when the component rendered before the home component was scrolled

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,22 +3,25 @@ import './Home.scss';
 import features from './features';
 import Card from '../Card/Card';
 
-const Home = () => (
-  <main className="home flex">
-    {
-      features.map(feature => (
-        <Card
-          link={ feature.link }
-          color={ feature.color }
-          heading={ feature.heading }
-          imgSrc={ feature.imgSrc }
-          imgAlt={ feature.imgAlt }
-          description={ feature.description }
-          key={ feature.heading }
-        />
-      ))
-    }
-  </main>
-);
+const Home = () => {
+  scroll(0, 0);
+  return (
+    <main className="home flex">
+      {
+        features.map(feature => (
+          <Card
+            link={ feature.link }
+            color={ feature.color }
+            heading={ feature.heading }
+            imgSrc={ feature.imgSrc }
+            imgAlt={ feature.imgAlt }
+            description={ feature.description }
+            key={ feature.heading }
+          />
+        ))
+      }
+    </main>
+  );
+};
 
 export default Home;
