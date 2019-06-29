b46f0e0b0d21d580a12fcd4f54c609972ef04d61
clintonpi <nkwochaclinton@gmail.com>
2019-06-29 14:11:41 +0100

    feat(client): add disqus feature

--- client/src/components/QAndA/QAndA.js
+++ client/src/components/QAndA/QAndA.js
@@ -1,6 +1,36 @@
-import React from 'react';
-import ComingSoon from '../ComingSoon/ComingSoon';
+import React, { Component, Fragment } from 'react';
+import { DiscussionEmbed } from 'disqus-react';
+import { documentTitle } from '../../utils';
 
-const QAndA = () => <ComingSoon title="Questions And Answers" />;
+class QAndA extends Component {
+  constructor(props) {
+    super(props);
+
+    this.title = 'Questions And Answers';
+  }
+
+  componentDidMount() {
+    document.title = `${documentTitle}${this.title}`;
+    scroll(0, 0);
+  }
+
+  render() {
+    const disqusShortname = 'versitails';
+    const disqusConfig = {
+      url: 'https://versitails.herokuapp.com',
+      identifier: 'QandA',
+      title: this.title
+    };
+
+    return (
+      <Fragment>
+        <h2 className="main-text">{ this.title }</h2>
+        <main>
+          <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+        </main>
+      </Fragment>
+    );
+  }
+}
 
 export default QAndA;
