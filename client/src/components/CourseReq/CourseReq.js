ca491d45d3424823b0b22ac6e95db6d34ca01575
clintonpi <nkwochaclinton@gmail.com>
2019-06-30 16:46:08 +0100

    feat(client): update course requirements file

--- client/src/components/CourseReq/CourseReq.js
+++ client/src/components/CourseReq/CourseReq.js
@@ -22,9 +22,9 @@ class CourseReq extends Component {
           <button className="m-b-20 p-10 bc-c1 ol-0 bd-0 bd-r-20 bx-sh sub-text fs-in pointer ttn-3" onClick={ () => this.courseReqDoc.current.requestFullscreen() }>Use fullscreen</button>
           <div className="flex" style={{ height: '50vh' }}>
             <iframe
-              src="/public/Course Requirements 2017-2018.pdf"
+              src="/public/Course Requirements 2018-2019.pdf"
               allow="fullscreen"
-              title="Course Requirements 2017/2018"
+              title="Course Requirements 2018/2019"
               ref={ this.courseReqDoc }
               className="w-100 bc-c1"
             />
