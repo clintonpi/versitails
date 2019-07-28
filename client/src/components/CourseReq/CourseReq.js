6253004a229fe2d789bfcf43cf311ee9f1716208
clintonpi <nkwochaclinton@gmail.com>
2019-07-28 08:55:37 +0100

    fix iframe overflow

--- client/src/components/CourseReq/CourseReq.js
+++ client/src/components/CourseReq/CourseReq.js
@@ -20,7 +20,7 @@ class CourseReq extends Component {
         <h2 className="main-text">{ this.title }</h2>
         <main className="flex column">
           <button className="m-b-20 p-10 bc-c1 ol-0 bd-0 bd-r-20 bx-sh sub-text fs-in pointer ttn-3" onClick={ () => this.courseReqDoc.current.requestFullscreen() }>Use fullscreen</button>
-          <div className="flex" style={{ height: '50vh' }}>
+          <div className="flex" style={{ height: '50vh', overflow: 'hidden' }}>
             <iframe
               src="/public/Course Requirements 2019-2020.pdf"
               allow="fullscreen"
