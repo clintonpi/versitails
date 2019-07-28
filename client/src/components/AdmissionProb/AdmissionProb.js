3c4cfd29123a5b8c467c839e4fd91411cf72026e
clintonpi <nkwochaclinton@gmail.com>
2019-07-28 11:35:57 +0100

    fix select elements appearing tiny on some browsers

--- client/src/components/AdmissionProb/AdmissionProb.js
+++ client/src/components/AdmissionProb/AdmissionProb.js
@@ -71,21 +71,21 @@ class AdmissionProb extends Component {
           <p className="sub-text w-100 p-10 m-b-20 bc-c1 bd-r-5 bx-sh-fx ttn-3 break-word">{ this.state.message }</p>
           <form onSubmit={ e => this.getProbability(e) } className="flex column m-t-20">
             <label htmlFor="faculty" className="sub-text">Select your faculty</label>
-            <select id="faculty" autoFocus onChange={ e => this.handleFacultySelect(e) } className="w-100 p-10 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text ttn-3 pointer">
+            <select id="faculty" autoFocus onChange={ e => this.handleFacultySelect(e) } className="w-100 p-5 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text fs-in ttn-3 pointer">
               {
                 Object.keys(faculties).map(faculty => <option key={ faculty }>{ faculty }</option>)
               }
             </select>
             <label htmlFor="course" className="sub-text">Select your course</label>
-            <select id="course" onChange={ e => this.setState({ selectedCourse: e.target.value }) } className="w-100 p-10 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text ttn-3 pointer">
+            <select id="course" onChange={ e => this.setState({ selectedCourse: e.target.value }) } className="w-100 p-5 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text fs-in ttn-3 pointer">
               {
                 Object.values(faculties[this.state.selectedFaculty])
                   .map(course => <option key={ course }>{ course }</option>)
               }
             </select>
             <label htmlFor="aggregate" className="sub-text">Enter your aggregate</label>
-            <input type="number" min="0" max="100" step="0.01" id="aggregate" onChange={ e => this.setState({ aggregate: e.target.value }) } className="w-100 p-10 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text txt-al-c ttn-3" required />
-            <input type="submit" value="Check" className="w-100 p-10 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text ttn-3 pointer" />
+            <input type="number" min="0" max="100" step="0.01" id="aggregate" onChange={ e => this.setState({ aggregate: e.target.value }) } className="w-100 p-5 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text fs-in txt-al-c ttn-3" required />
+            <input type="submit" value="Check" className="w-100 p-5 m-t-10 m-b-20 bd-0 ol-0 bd-r-20 bx-sh bc-c1 main-text fs-in ttn-3 pointer" />
           </form>
         </main>
       </Fragment>
